c7fb8201026b6e27d0a261ca5700af90de67ca90
Aurelio Killian <aurelioklv@gmail.com>
2024-11-10 12:28:36 +0700

    update navbar

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,34 +1,8 @@
-import {IconComponents, IconDashboard,} from "@tabler/icons-react";
+import {IconMail, IconMailPlus, IconMailQuestion} from "@tabler/icons-react";
 import type {NavItem} from "@/types/nav-item";
 
 export const navLinks: NavItem[] = [
-	{label: "Dashboard", icon: IconDashboard, link: "/dashboard"},
-
-	{
-		label: "Components",
-		icon: IconComponents,
-		initiallyOpened: true,
-		links: [
-			{
-				label: "Table",
-				link: "/dashboard/table",
-			},
-			{
-				label: "Form",
-				link: "/dashboard/form",
-			},
-			{
-				label: "Surat Saya",
-				link: "/dashboard/surat",
-			},
-			{
-				label: "Tambah Surat",
-				link: "/dashboard/surat/tambah",
-			},
-			{
-				label: "Spare Surat",
-				link: "/dashboard/surat/spare",
-			},
-		],
-	},
+	{label: "Surat Saya", icon: IconMail, link: "/dashboard/surat"},
+	{label: "Tambah Surat", icon: IconMailPlus, link: "/dashboard/surat/tambah"},
+	{label: "Spare Surat", icon: IconMailQuestion, link: "/dashboard/surat/spare"},
 ];
