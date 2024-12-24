547b14f8309d932c958a26c177b3ec27b6086602
jawahirulwildan <jawahirulwildan@gmail.com>
2024-12-24 16:59:42 +0700

    modify UpdateLetterForm and services/surat
    Add update column for Hak Akses, Index Nama Berkas, Jangka Simpan Waktu Aktif, Jangka Simpan Waktu Inaktif, Keterangan di JRA, and Lokasi Simpan.
    
    Add all new variables to append in patchLetter function.
    
    Add all new variables to UpdateLetterResponse interface.

--- src/services/surat/index.ts
+++ src/services/surat/index.ts
@@ -81,6 +81,12 @@ export const patchLetter = async (id: string, formData: UpdateLetterResponse): P
         formDataToSend.append('levelId', formData.levelId);
         formDataToSend.append('attachmentCount',formData.attachmentCount)
         formDataToSend.append('description', formData.description);
+        formDataToSend.append('accessId', formData.accessId);
+        formDataToSend.append('documentIndexName', formData.documentIndexName);
+        formDataToSend.append('activeRetentionPeriodId', formData.activeRetentionPeriodId);
+        formDataToSend.append('inactiveRetentionPeriodId', formData.inactiveRetentionPeriodId);
+        formDataToSend.append('jraDescriptionId', formData.jraDescriptionId);
+        formDataToSend.append('storageLocationId', formData.storageLocationId);
         if (formData.file) formDataToSend.append('file', formData.file);
 
         await ky.patch(`${BASE_URL}/${id}`, {
