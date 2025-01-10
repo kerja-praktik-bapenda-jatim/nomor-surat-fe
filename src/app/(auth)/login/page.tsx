a4f5b5f98d753b5bd65182fa1b64f0f2e26e51ae
jawahirulwildan <jawahirulwildan@gmail.com>
2025-01-10 21:26:55 +0700

    combine layout.tsx in auth to login/page.tsx

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,5 +1,27 @@
+"use client";
+
+import { Box, Title, Text, Image } from "@mantine/core";
 import { LoginForm } from "@/components/Auth/LoginForm";
+import { useAuthRedirect } from "@/services/auth";
+import classes from "../layout.module.css";
 
 export default function Login() {
-  return <LoginForm />;
+  useAuthRedirect();
+
+  return (
+    <Box className={classes.wrapper}>
+      <Image h={170} w="auto" fit="contain" src="/bg.png" />
+
+      <Title order={1} fw={700} ta="center" mt={25}>
+        SINORAT
+        <Text fw={100} size="sm">
+          Sistem Informasi Surat Bapenda Jatim
+        </Text>
+      </Title>
+
+      <Box w={400}>
+        <LoginForm />
+      </Box>
+    </Box>
+  );
 }
