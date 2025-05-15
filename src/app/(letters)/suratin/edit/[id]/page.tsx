f4bdc40d85476246879e55f14d4b07b8c7dddcf0
ranabel <nabilahatikarahma26@gmail.com>
2025-05-15 18:10:38 +0700

    add update page and component form

---
+++ src/app/(letters)/suratin/edit/[id]/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+import { UpdateLetterForm } from "@/components/Form/Suratin/UpdateLetterForm";
+import { PageContainer } from "@/components/PageContainer/PageContainer";
+
+export default function EditSuratPage() {
+    return (
+        <PageContainer title="">
+            <UpdateLetterForm />
+        </PageContainer>
+    );
+}
